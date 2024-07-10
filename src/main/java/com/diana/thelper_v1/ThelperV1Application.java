7551ce9609ca92ebd478df165cc33f530d5c827b
dianaaugusta <joaolimah406@gmail.com>
2024-07-10 20:17:13 -0300

    first commit

---
+++ src/main/java/com/diana/thelper_v1/ThelperV1Application.java
@@ -0,0 +1,13 @@
+package com.diana.thelper_v1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThelperV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThelperV1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/diana/thelper_v1/ThelperV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diana.thelper_v1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThelperV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
